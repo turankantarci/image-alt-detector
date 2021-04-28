f4a7e589146b2b4ca73773e24765f11a16170cf3
Turan Kantarcı <hanedanturan@gmail.com>
2021-04-28 03:00:05 +0300

    Feature - image sources

--- app/content.js
+++ app/content.js
@@ -1,7 +1,7 @@
 const app = {
     detectedElements: [],
     checkElement(element) {
-        if (!element.hasAttribute('alt') || !element.getAttribute('alt')) {
+        if ((!element.hasAttribute('alt') || !element.getAttribute('alt')) && (element.hasAttribute('src') && element.getAttribute('src'))) {
             element.classList.add('image-alt-detector-element');
             this.detectedElements.push(element);
         };
@@ -16,7 +16,10 @@ const app = {
                                 <div class="close-image-alt-detector" id="closeImageAltDetector">
                                     <img src="${chrome.runtime.getURL('assets/close-icon.svg')}" />
                                 </div>
-                            </div>`;
+                            </div>
+                            <textarea class="src-list" id="sourceList">${(this.detectedElements.map(element => element.src)).join('\n')}</textarea>
+                            <button type="button" class="copy-sources-button" id="copySources">Copy Image Sources</button>
+                            `;
     },
     detectAlt() {
         let images = document.querySelectorAll('img');
@@ -27,6 +30,10 @@ const app = {
             this.createBar();
             this.events();
         }
+    },
+    copySources() {
+        document.querySelector('#sourceList').select();
+        document.execCommand('copy');
     },
     closeApp() {
         this.detectedElements.forEach((element) => {
@@ -39,6 +46,11 @@ const app = {
         // Close app
         document.querySelector('#closeImageAltDetector').addEventListener('click', () => {
             this.closeApp();
+        });
+
+        // Copy sources
+        document.querySelector('#copySources').addEventListener('click', () => {
+            this.copySources();
         });
     },
     init() {
