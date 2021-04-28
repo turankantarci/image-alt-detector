0675d52d096ba3806496f168a36c06c581386d1c
Turan Kantarcı <hanedanturan@gmail.com>
2021-04-29 01:18:08 +0300

    "all is well" message added

--- app/content.js
+++ app/content.js
@@ -6,21 +6,6 @@ const app = {
             this.detectedElements.push(element);
         };
     },
-    createBar() {
-        let barMain = document.createElement("div");
-        barMain.id = 'imageAltDetectorApp';
-        barMain.classList.add('image-alt-detector-bar');
-        document.body.insertBefore(barMain, document.body.firstChild);
-        barMain.innerHTML = `<div class="detected-element-text">
-                                ${this.detectedElements.length} image ${this.detectedElements.length > 1 ? "elements" : "element"} found there ${this.detectedElements.length > 1 ? "are" : "is"} no "alt" text or attribute
-                                <div class="close-image-alt-detector" id="closeImageAltDetector">
-                                    <img src="${chrome.runtime.getURL('assets/close-icon.svg')}" />
-                                </div>
-                            </div>
-                            <textarea class="src-list" id="sourceList">${(this.detectedElements.map(element => element.src)).join('\n')}</textarea>
-                            <button type="button" class="copy-sources-button" id="copySources">Copy Image Sources</button>
-                            `;
-    },
     detectAlt() {
         let images = document.querySelectorAll('img');
         images.forEach(element => {
@@ -29,7 +14,24 @@ const app = {
         if (this.detectedElements.length) {
             this.createBar();
             this.events();
+        } else {
+            this.allIsWell();
         }
+    },
+    createBar() {
+        let barMain = document.createElement("div");
+        barMain.id = 'imageAltDetectorApp';
+        barMain.classList.add('image-alt-detector-bar');
+        document.body.insertBefore(barMain, document.body.firstChild);
+        barMain.innerHTML = `<div class="detected-element-text">
+                                ${this.detectedElements.length} image ${this.detectedElements.length > 1 ? "elements" : "element"} found there ${this.detectedElements.length > 1 ? "are" : "is"} no "alt" text or attribute
+                                <div class="close-image-alt-detector" id="closeImageAltDetector">
+                                    <img src="${chrome.runtime.getURL('assets/close-icon.svg')}" alt="Close" />
+                                </div>
+                            </div>
+                            <textarea class="src-list" id="sourceList">${(this.detectedElements.map(element => element.src)).join('\n')}</textarea>
+                            <button type="button" class="copy-sources-button" id="copySources">Copy Image Sources</button>
+                            `;
     },
     copySources() {
         document.querySelector('#sourceList').select();
@@ -41,6 +43,14 @@ const app = {
         });
         document.querySelector('#imageAltDetectorApp').remove();
         this.detectedElements = [];
+    },
+    allIsWell() {
+        let messageHtml = document.createElement('div');
+        messageHtml.classList.add('image-alt-detector', 'all-is-well-message');
+        messageHtml.innerHTML = 'All is well! :)';
+        document.body.insertBefore(messageHtml, document.body.firstChild);
+
+        setTimeout(() => messageHtml.remove(), 3000);
     },
     events() {
         // Close app
